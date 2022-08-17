d68ec8277cecdb1635af105feb890983101bad96
FranSemprini <fransemprini04@gmail.com>
2022-08-17 17:19:28 -0400

    added responsive design

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -7,14 +7,59 @@ import Toolbar from "@mui/material/Toolbar";
 import Avatar from '@mui/material/Avatar';
 import Typography from "@mui/material/Typography";
 import logo from '../img/logo.jpg'
+import Menu from '@mui/material/Menu';
+import MenuItem from '@mui/material/MenuItem';
+import IconButton from "@mui/material/IconButton";
+import MenuIcon from "@mui/icons-material/Menu";
 
 
 
 export function NavBar() {
+  const [anchorEl, setAnchorEl] = React.useState(null);
+  const open = Boolean(anchorEl);
+  const handleClick = (event) => {
+    setAnchorEl(event.currentTarget);
+  };
+  const handleClose = () => {
+    setAnchorEl(null);
+  };
+
   return (
-    <AppBar position="static" sx={{ color: 'ligth-blue', backgroundColor: 'orange', borderColor: 'amber'}}>
+    <AppBar position="static" sx={{ color: 'ligth-blue', backgroundColor: 'orange', borderColor: 'amber' }}>
       <Container maxWidth="xl">
-        <Toolbar disableGutters sx={{justifyContent: 'space-between'}}>
+        <Toolbar disableGutters sx={{ justifyContent: 'space-between' }}>
+          <Box sx={{ display: { xs: 'flex', sm: 'none' } }}>
+            <IconButton
+              size="large"
+              aria-label="account of current user"
+              aria-controls={open ? 'menu' : undefined}
+              aria-haspopup="true"
+              aria-expanded={open ? 'true' : undefined}
+              onClick={handleClick}
+              color="inherit"
+            >
+              <MenuIcon />
+            </IconButton>
+            <Menu
+              id="menu"
+              aria-labelledby="menuButton"
+              anchorEl={anchorEl}
+              open={open}
+              onClose={handleClose}
+              anchorOrigin={{
+                vertical: 'top',
+                horizontal: 'left',
+              }}
+              transformOrigin={{
+                vertical: 'top',
+                horizontal: 'left',
+              }}
+            >
+              <MenuItem onClick={handleClose}>Home</MenuItem>
+              <MenuItem onClick={handleClose}>Gallery</MenuItem>
+              <MenuItem onClick={handleClose}>AboutUs</MenuItem>
+            </Menu>
+          </Box>
           <Box sx={{ display: 'flex', alignItems: 'center' }}>
             <Avatar alt="Remy Sharp" src={logo} />
             <Typography
@@ -22,8 +67,8 @@ export function NavBar() {
               component="a"
               sx={{
                 ml: 1,
-                mt:1,
-                fontSize: 35,
+                mt: 1,
+                fontSize: 32,
                 fontFamily: `gabriola`,
                 fontWeight: 700,
                 letterSpacing: ".1rem",
@@ -34,32 +79,45 @@ export function NavBar() {
               Kittens Art!
             </Typography>
           </Box>
-          <Box sx={{}}>
-            <Button variant="contained" sx={{
-              color: 'black', backgroundColor: 'white', borderColor: 'black', mr: 5, '&:hover': {
-                color: `white`,
-                background: "#834bff",
-              },
-            }}>Home</Button>
+          <Box sx={{ display: { xs: 'none', sm: 'flex', md: 'flex' } }}>
+            <div>
+              <Button variant="contained" sx={{
+                color: 'black', backgroundColor: 'white', borderColor: 'black', mr: 4, '&:hover': {
+                  color: `white`,
+                  background: "#834bff",
+                  display: { xs: 'none', sm: 'flex', md: 'flex' }
+                },
+              }}>Home</Button>
+              <Button variant="text" size="small" sx={{
+                color: 'white', backgroundColor: 'orange', borderColor: 'black', ml: 3, '&:hover': {
+                  color: `white`,
+                  background: "#834bff",
+                },
+              }}>Gallery</Button>
+              <Button variant="text" size="small" sx={{
+                color: 'white', backgroundColor: 'orange', borderColor: 'black', ml: 3, '&:hover': {
+                  color: `white`,
+                  background: "#834bff",
+                },
+              }}>About us</Button>
+            </div>
             <Button variant="text" size="small" sx={{
-              color: 'white', backgroundColor: 'orange', borderColor: 'black', ml: 4, '&:hover': {
-                color: `white`,
-                background: "#834bff",
-              },
-            }}>Gallery</Button>
-            <Button variant="text" size="small" sx={{
-              color: 'white', backgroundColor: 'orange', borderColor: 'black', ml: 4, '&:hover': {
-                color: `white`,
-                background: "#834bff",
-              },
-            }}>About us</Button>
-            <Button variant="text" size="small" sx={{
-              color: 'white', backgroundColor: 'orange', borderColor: 'black', ml: 4, '&:hover': {
+              color: 'white', backgroundColor: 'orange', borderColor: 'black', ml: 3, '&:hover': {
                 color: `white`,
                 background: "#834bff",
               },
             }}>LogIn</Button>
           </Box>
+
+          <Box sx={{ display: { xs: 'flex', sm: 'none' } }}>
+            <Button variant="text" size="small" sx={{
+              color: 'white', backgroundColor: 'orange', borderColor: 'black', '&:hover': {
+                color: `white`,
+                background: "#834bff",
+              },
+            }}>LogIn</Button>
+          </Box>
+
         </Toolbar>
       </Container>
     </AppBar>
